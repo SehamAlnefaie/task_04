1869992a02a3c33a1b48a9cf82183ed0cc1491ef
SehamAlnefaie <seham.alnufaii@yahoo.com>
2019-07-23 19:19:52 +0300

    solution

--- restaurants/models.py
+++ restaurants/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from datetime import datetime 
 # Create your models here.
+class Restaurant (models.Model):
+
+    name= models.CharField(max_length=20)
+    description = models.TextField()
+    opening_time = models.TimeField()
+    closing_time = models.TimeField()
+
+    def __str__(self):
+       return self.name
+
